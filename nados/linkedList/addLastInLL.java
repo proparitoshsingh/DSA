4155447e69125adb1ffb4bbda3118101ea086bd4
Paritosh Singh <paritoshsingh1609@gmail.com>
2022-08-26 12:12:30 +0530

    adding node in last of the linked list

---
+++ nados/linkedList/addLastInLL.java
@@ -0,0 +1,38 @@
+import java.util.Scanner; 
+public class addLastInLL {
+    
+    public static class Node{
+        int data;
+        Node next;
+    }
+    public static class LinkedList{
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int data){
+            Node temp=new Node();
+            temp.data=data;
+            if(this.size==0){
+                this.head=temp;
+                this.tail=temp;
+            }else{
+                this.tail.next=temp;
+                this.tail=temp;
+            }this.size++;
+        }
+        void display(){
+            for(Node temp=this.head;temp!=null;temp=temp.next){
+                System.out.println(temp.data);
+            }
+        }
+    }
+
+    public static void main(String args[]) { 
+        LinkedList ll=new LinkedList();
+        ll.addLast(10);
+        ll.addLast(20);
+        ll.addLast(30);
+        ll.display();
+    } 
+}
