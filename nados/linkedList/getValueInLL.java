94366159d89dfa0b11de50dee310ae704e849ee3
Paritosh Singh <paritoshsingh1609@gmail.com>
2022-08-26 14:59:16 +0530

    get first ,last, node at given index in linked list

---
+++ nados/linkedList/getValueInLL.java
@@ -0,0 +1,66 @@
+import java.util.Scanner; 
+public class getValueInLL { 
+
+    public static class Node{
+        int data;
+        Node next;
+    }
+    public static class LinkedList{
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int data){
+            Node temp=new Node();
+            temp.data=data;
+            if(this.size==0){
+                this.head=temp;
+                this.tail=temp;
+            }else{
+                this.tail.next=temp;
+                this.tail=temp;
+            }this.size++;
+        }
+        void display(){
+            for(Node temp=this.head;temp!=null;temp=temp.next){
+                System.out.println(temp.data);
+            }
+        }
+        int getFirst(){
+            if(size==0){
+                System.out.println("List is empty");
+                return -1;
+            }return this.head.data;
+        }
+      
+        int getLast(){
+            if(size==0){
+                System.out.println("List is empty");
+                return -1;
+            }return this.tail.data;
+        }
+      
+        int getAt(int idx){
+            if(idx>=size){
+                System.out.println("Invalid arguments");
+                return -1;
+            }if(size==0){
+                System.out.println("List is empty");
+                return -1;
+            }Node ptr=this.head;
+            for(int i=0;i<idx;i++){
+                ptr=ptr.next;
+            }return ptr.data;
+        }
+    }
+    public static void main(String args[]) { 
+         LinkedList ll=new LinkedList();
+         ll.addLast(10);
+         ll.addLast(20);
+         ll.addLast(30);
+         System.out.println(ll.getFirst());
+         System.out.println(ll.getLast());
+         System.out.println(ll.getAt(2));
+         System.out.println(ll.getAt(4));
+    } 
+}
