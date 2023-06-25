e09fb685e6dce22a9d299ac6b8132ce1c478da78
Paritosh Singh <paritoshsingh1609@gmail.com>
2023-06-25 17:21:13 +0530

    add two number represented in linkedlist

---
+++ SDEsheet/LinkedList/addTowNums.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }carry = sum / 10;
+            ListNode newNode = new ListNode(sum % 10);
+            curr.next = newNode;
+            curr = curr.next;
+        }if (carry > 0) {
+            ListNode newNode = new ListNode(carry);
+            curr.next = newNode;
+        }return dummy.next;
+    }
+}
