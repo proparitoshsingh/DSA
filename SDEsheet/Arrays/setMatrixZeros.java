ad50ba4413c58b25a72ad1c70a21617fdf3ff767
Paritosh Singh <paritoshsingh1609@gmail.com>
2024-05-11 23:49:50 +0530

    better approach

--- SDEsheet/Arrays/setMatrixZeros.java
+++ SDEsheet/Arrays/setMatrixZeros.java
@@ -29,3 +29,58 @@ class Solution {
         }
     }
 }
+
+//event better approach that saves the extra space which is used in hashmap approach 
+
+
+// class Solution {
+//     public void setZeroes(int[][] matrix) {
+//         boolean rowFlag = false, colFlag = false;
+//         for (int i = 0; i < matrix.length; i++) {
+//             if (matrix[i][0] == 0)
+//                 colFlag = true;
+//         }
+
+//         for (int i = 0; i < matrix[0].length; i++) {
+//             if (matrix[0][i] == 0)
+//                 rowFlag = true;
+//         }
+
+//         for (int i = 1; i < matrix.length; i++) {
+//             for (int j = 1; j < matrix[0].length; j++) {
+//                 if (matrix[i][j] == 0) {
+//                     matrix[0][j] = 0;
+//                     matrix[i][0] = 0;
+//                 }
+//             }
+//         }
+
+//         for (int i = 1; i < matrix[0].length; i++) {
+//             if (matrix[0][i] == 0) {
+//                 for (int j = 1; j < matrix.length; j++) {
+//                     matrix[j][i] = 0;
+//                 }
+//             }
+//         }
+
+//         for (int i = 1; i < matrix.length; i++) {
+//             if (matrix[i][0] == 0) {
+//                 for (int j = 1; j < matrix[0].length; j++) {
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+
+//         if (rowFlag) {
+//             for (int i = 0; i < matrix[0].length; i++) {
+//                 matrix[0][i] = 0;
+//             }
+//         }
+
+//         if (colFlag) {
+//             for (int i = 0; i < matrix.length; i++) {
+//                 matrix[i][0] = 0;
+//             }
+//         }
+//     }
+// }
